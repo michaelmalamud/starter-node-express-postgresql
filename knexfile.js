79b5226043dd5ad9c2e1b284bfd7dc916acb4dbc
Michael Malamud <michael.malamud@outlook.com>
2021-05-12 09:06:13 -0500

    connecting to database with Knex

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,17 @@
+//require dotenv 
+
+require("dotenv").config();
+
+//stores url from process.env
+
+const DATABASE_URL = process.env;
+
 // Update with your config settings.
 
 module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: '',
+    connection: DATABASE_URL,
   },
 };
---
+++ src/db/fixtures/connection.js
@@ -0,0 +1,15 @@
+//determines environment where code is running and stores it in env variable
+
+const env = process.env.NODE_ENV || "development";
+
+//reqires database configuration object from knexfile and stores in config var
+
+const config = require("../../knexfile")[env];
+
+//initializes a Knex instance by calling the knex module, passing in config as an argument.
+
+const knex = require("knex")(config);
+
+//exports knex instance for use by other files 
+
+module.exports = knex;
