bb35e41a45aa56deafb4527957672358e82c5e21
Michael Malamud <michael.malamud@outlook.com>
2021-05-13 10:27:15 -0500

    asyncErrorBoundary and aggregates

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,5 +1,34 @@
 const knex = require("../db/fixtures/connection");
 
+function listTotalWeightByProduct() {
+  return knex("products")
+    .select(
+      "product_sku",
+      "product_title",
+      knex.raw(
+        "sum(product_weight_in_lbs * product_quantity_in_stock) as total_weight_in_lbs"
+      )
+    )
+    .groupBy("product_title", "product_sku");
+}
+
+function listPriceSummary() {
+  return knex("products")
+    .select("supplier_id")
+    .min("product_price")
+    .max("product_price")
+    .avg("product_price")
+    .groupBy("supplier_id");
+}
+
+function listOutOfStockCount() {
+  return knex("products")
+    .select("product_quantity_in_stock as out_of_stock")
+    .count("product_id")
+    .where({ product_quantity_in_stock: 0 })
+    .groupBy("out_of_stock");
+}
+
 function read(product_id) {
     return knex("products").select("*").where({ product_id }).first();
   }
@@ -10,5 +39,8 @@ function list() {
 
 module.exports = {
   list,
-  read
+  read,
+  listOutOfStockCount,
+  listPriceSummary,
+  listTotalWeightByProduct,
 };
